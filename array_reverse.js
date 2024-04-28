5fdab7e9372d4ef93be6e26cdc68a075f72e440b
haneefyaseen <mdhaneefyaseen@gmail.com>
2024-04-28 14:11:56 +0530

    Initial commit

--- array_reverse.js
+++
@@ -1,5 +0,0 @@
-module.exports = function (a) {
-    for (let i = a.length-1; i >=0; i--) {
-        console.log(a[i])
-    }
-}
--- hello.js
+++
@@ -1,4 +0,0 @@
-const rev = require('./array_reverse');
-let a =  [1, 2, 3, 5]
-
-rev(a);
